8a45bc99fd778adf4efc004b88bc0fd19ef8eb44
juhincapie <juhincapie@unal.edu.co>
2022-12-05 14:46:34 -0500

    Solution 2

--- preguntas.py
+++ preguntas.py
@@ -1,6 +1,6 @@
 """
 Clasificación usando k-NN - Digits Dataset
------------------------------------------------------------------------------------------
+--------------------------------------------- --------------------------------------------
 En este laboratio se construirá un clasificador usando k-NN para el dataset de digitos.
 """
 import matplotlib.pyplot as plt
